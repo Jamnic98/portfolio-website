b272e6918a70c949272bd1cc67c424e743e1d38d
Jamnic98 <jamiestimpson30@gmail.com>
2021-02-01 10:12:02 +0000

    "removed tilt animation"

--- src/components/card/Card.js
+++ src/components/card/Card.js
@@ -1,10 +1,25 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { FaJava } from 'react-icons/fa';
 import { DiJavascript1, DiPython } from 'react-icons/di';
 
+import Tilt from 'react-tilt';
+
 import './Card.css';
 
+
 function Card({ title, imgUrl, description, ghLink, technologies }) {
+
+    const options = {
+        reverse: true,  // reverse the tilt direction
+        max: 10,     // max tilt rotation (degrees)
+        perspective: 1000,   // Transform perspective, the lower the more extreme the tilt gets.
+        scale: 1,      // 2 = 200%, 1.5 = 150%, etc..
+        speed: 300,    // Speed of the enter/exit transition
+        transition: true,   // Set a transition on enter/exit.
+        axis: null,   // What axis should be disabled. Can be X or Y.
+        reset: true,    // If the tilt effect has to be reset on exit.
+        easing: "cubic-bezier(.03,.98,.52,.99)",    // Easing on enter/exit.
+    }
 
     const iconMap = {
         "python": <DiPython />,
@@ -26,14 +41,26 @@ function Card({ title, imgUrl, description, ghLink, technologies }) {
         return iconList;
     }
 
+    const handleMouseMove = () => {
+    }
+
+    const handleMouseOver = () => {
+        setFocus()
+    }
+
+    const handleMouseOut = () => {
+        setFocus()
+    }
+
     return (
-        <a className="card"
+        <a
             href={ghLink}
             target="_blank"
-            onMouseOver={() => setFocus()}
-            onMouseOut={() => setFocus()}
+            onMouseOver={(e) => handleMouseOver(e)}
+            onMouseOut={(e) => handleMouseOut(e)}
+            className="card"
         >
-            <div className="img-container">
+            <div className="card-img-container">
                 <div className="color-overlay">
                     <img className={`card-img ${isFocused ? "card-img-zoom" : ""}`} src={imgUrl} />
                 </div>
--- src/components/card/cardData.js
+++ src/components/card/cardData.js
@@ -12,37 +12,7 @@ const cards = [
     }, {
         title: "Pyramid Solitaire",
         description: `A game based on a variation of the popular card game, solitaire.`,
-        ghLink: "https://github.com/Jamnic98/pyramid-solitaire/blob/master/README.md",
-        screenshots: [
-            {
-                url: "/images/pyramid-solitaire.png "
-            }
-        ],
-        technologies: ["python"]
-    }, {
-        title: "Reading Log",
-        description: `A list of books the user has read, stored in a MySQL database.`,
-        ghLink: "https://github.com/Jamnic98/reading-log/blob/main/README.md",
-        screenshots: [
-            {
-                url: "/images/reading-log.jpg "
-            }
-        ],
-        technologies: ["java"]
-    }, {
-        title: "Scrabble Scorekeeper",
-        description: `A web-based program designed to calculate player scores in Scrabble.`,
-        ghLink: "https://github.com/Jamnic98/scrabble-scorekeeper/blob/master/README.md",
-        screenshots: [
-            {
-                url: "/images/scrabble-tiles.png "
-            }
-        ],
-        technologies: ["javaScript"]
-    }, {
-        title: "Pyramid Solitaire",
-        description: `A game based on a variation of the popular card game, solitaire.`,
-        ghLink: "https://github.com/Jamnic98/pyramid-solitaire/blob/master/README.md",
+        ghLink: "https://github.com/Jamnic98/pyramid-solitaire/blob/main/README.md",
         screenshots: [
             {
                 url: "/images/pyramid-solitaire.png "
