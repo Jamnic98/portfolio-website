0cb4df661d0490e32fef14303b3cf4d8d6fb6dd0
Jamnic98 <jamiestimpson30@gmail.com>
2021-06-30 15:50:15 +0100

    updated github link href

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,5 +1,12 @@
 import { NavLink } from 'react-router-dom';
-import { FaHome, FaPencilRuler, FaBars, FaTimes, FaGithub, FaLinkedin } from 'react-icons/fa';
+import {
+  FaHome,
+  FaPencilRuler,
+  FaBars,
+  FaTimes,
+  FaGithub,
+  FaLinkedin,
+} from 'react-icons/fa';
 import { MdEmail } from 'react-icons/md';
 import { useState } from 'react';
 import Dropdown from '../dropdown/Dropdown';
@@ -7,7 +14,6 @@ import Dropdown from '../dropdown/Dropdown';
 import navStyles from './Navbar.module.css';
 
 function Navbar() {
-
   const [clicked, setClicked] = useState(false);
   const [dropdown, setDropdown] = useState(false);
 
@@ -25,16 +31,22 @@ function Navbar() {
     } else {
       setDropdown(true);
     }
-  }
+  };
 
   const handleProjectsClick = () => {
-    handleDropdownClick()
-    closeMobileMenu()
-  }
+    handleDropdownClick();
+    closeMobileMenu();
+  };
 
   const setDropdownComponent = () => {
-
-    const projectNavItem = <div><div className={navStyles.icon}><FaPencilRuler /></div><span>Projects</span></div>
+    const projectNavItem = (
+      <div>
+        <div className={navStyles.icon}>
+          <FaPencilRuler />
+        </div>
+        <span>Projects</span>
+      </div>
+    );
 
     if (window.innerWidth < 400) {
       return (
@@ -42,60 +54,71 @@ function Navbar() {
           to='/projects'
           onClick={handleProjectsClick}
           className={`${navStyles.navLink}`}
-
         >
           {projectNavItem}
         </NavLink>
       );
     } else {
       return (
-        <div
-          onClick={handleProjectsClick}
-          className={`${navStyles.navLink}`}
-        >
+        <div onClick={handleProjectsClick} className={`${navStyles.navLink}`}>
           {projectNavItem}
         </div>
       );
     }
-  }
+  };
 
   return (
     <nav className={`${navStyles.navbar}`}>
       <div onClick={handleClick} className={`${navStyles.menuIcon}`}>
         {clicked ? <FaTimes /> : <FaBars />}
       </div>
-      <ul className={`${navStyles.navMenu} ${clicked ? `${navStyles.active}` : ''}`}>
+      <ul
+        className={`${navStyles.navMenu} ${
+          clicked ? `${navStyles.active}` : ''
+        }`}
+      >
         <li className={`${navStyles.navItem}`}>
-          <NavLink exact
+          <NavLink
+            exact
             to='/'
             onClick={closeMobileMenu}
             className={`${navStyles.navLink}`}
           >
-            <div className={navStyles.icon}><FaHome /></div><span>Home</span>
+            <div className={navStyles.icon}>
+              <FaHome />
+            </div>
+            <span>Home</span>
           </NavLink>
         </li>
-        <li
-          onMouseLeave={handleMouseLeave}
-          className={`${navStyles.navItem}`}
-        >
+        <li onMouseLeave={handleMouseLeave} className={`${navStyles.navItem}`}>
           {setDropdownComponent()}
           {dropdown && <Dropdown />}
         </li>
         <li className={`${navStyles.navItem}`}>
-          <a href='mailto:jamiestimpson30@gmail.com' className={`${navStyles.navLink}`} >
-            <div className={navStyles.icon}><MdEmail /></div><span>Email</span>
+          <a
+            href='mailto:jamiestimpson30@gmail.com'
+            className={`${navStyles.navLink}`}
+          >
+            <div className={navStyles.icon}>
+              <MdEmail />
+            </div>
+            <span>Email</span>
           </a>
         </li>
 
         <li className={`${navStyles.navItem}`}>
           <a
-            href='https://github.com/Jamnic98/repositories'
+            href='https://github.com/Jamnic98?tab=repositories'
             target='_blank'
             rel='noreferrer'
             onClick={closeMobileMenu}
             className={`${navStyles.navLink}`}
           >
-            <div className={navStyles.icon}><span><FaGithub /></span></div>
+            <div className={navStyles.icon}>
+              <span>
+                <FaGithub />
+              </span>
+            </div>
           </a>
           <a
             href='https://www.linkedin.com/in/jamie-stimpson-23ab11203/'
@@ -104,10 +127,13 @@ function Navbar() {
             onClick={closeMobileMenu}
             className={`${navStyles.navLink}`}
           >
-            <div className={navStyles.icon}><span><FaLinkedin /></span></div>
+            <div className={navStyles.icon}>
+              <span>
+                <FaLinkedin />
+              </span>
+            </div>
           </a>
         </li>
-
       </ul>
     </nav>
   );
